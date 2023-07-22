240d7a32d7260493902aebcb97ab85333a238d98
Anushateggi123 <xworkzanusha@gmail.com>
2023-07-22 14:03:05 +0530

    test-push

---
+++ Test.java
@@ -0,0 +1,44 @@
+//java test
+
+1. 10,34
+2. Zudio
+3. 50
+4. sum is 200 length is 4
+6. Parameters are the datatypes which specified inside the method braces.you can pass values/arguments to the method parameter, at the method call
+7. compile time polymorphism is achieved through static binding. 
+example for compile time polymorphism is method overloading.
+In which method overloading is having same name but different in parameters.
+There are 3 ways in method overloading:
+changing number of parameters
+changing types of parameters
+changing order of parameters
+8. varible arguments  is a method which takes the variable number of values.varargs must be the last argument.
+Syntax of varargs is (arg)
+9. Packages is a group of similar kind of classes , subpackages and interfaces.
+There are two types in packages:
+Built-in packages
+User-defined packages
+package keyword is used to create package in java.
+Types of imports: 
+Implicit import : when we load all classes of package using * it will call as implicit package.
+Explicit import : classes are present inside the package, when we give proper path it will call as explicit import.
+10. Construtor Chaining : Calling constructor from one contructor to other constructor.
+we can achieve constructor by using this() and super().
+Where this() is which calls constructor from same class and super() which calls constructor from parent class.
+11. public class Fibonaci{
+		static int fib(int n)
+		if(n<=1){
+			return n;
+		return fib(n-1)+fib(n-2);
+		}
+		public static void main(String[] args){
+			int n=8;
+			System.out.println(fib(n))
+			}
+		
+}
+12. By using isListOdd(). we can also use for loop to check whether it is odd.
+14. Enumeration : enum is a keyword which is unchangeable type and containes group of constants.
+It is used to restrict the data.
+15. It allocates the static memory.
+16. Variables , methods , inner class , constructor
